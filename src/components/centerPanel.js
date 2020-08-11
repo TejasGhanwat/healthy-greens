e43fd71ff4404d9045da23e954e211a7fda98f7d
Tejas Ghanwat <tejas.aghanwat@gmail.com>
2020-08-11 13:02:33 -0700

    introduced the main components

---
+++ src/components/centerPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import CenterTop from "./centerTop";
+export class CenterPanel extends Component {
+  render() {
+    return (
+      <div className='centerPanel'>
+        <CenterTop />
+      </div>
+    );
+  }
+}
+
+export default CenterPanel;
