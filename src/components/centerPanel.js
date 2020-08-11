46f3e61b4b7214a097ce23783c295bc1f9d8ed60
Tejas Ghanwat <tejas.aghanwat@gmail.com>
2020-08-11 14:40:57 -0700

    added ingredients component

--- src/components/centerPanel.js
+++ src/components/centerPanel.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import CenterTop from "./centerTop";
+import Ingredients from "./ingredients";
 export class CenterPanel extends Component {
   render() {
     return (
       <div className='centerPanel'>
         <CenterTop />
+        <Ingredients />
       </div>
     );
   }
